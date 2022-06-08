f022faa04c4bf3acf324a52cc03d5fdaaa273ea0
Nindozane <80363019+Nindozane@users.noreply.github.com>
2022-06-08 11:34:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function setup() {
-    canvas = createCanvas(280, 280);
+    canvas = createCanvas(400, 400);
     canvas.center();
     background("white");
     canvas.mouseReleased(classifyCanvas);
